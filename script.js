9b9583879aabf2726129f12fc9b4e04a76bdb064
r4coder <ritheeshk2003@gmail.com>
2026-08-11 19:25:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+function updateTimer() {
+    let timer = document.getElementById("timer");
+    let now = new Date();
+
+    timer.textContent = now.toLocaleString();
+}
+
+updateTimer();
+
+setInterval(updateTimer, 1000);
